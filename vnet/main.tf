0490e3281fb62670fb1b4222ee104adc007e32af
dyronrh <dyronrh@yahoo.es>
2024-10-26 10:10:42 -0300

    add rg

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,7 +1,11 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "rg-stacks-${var.resource_group_name}"
+  location = var.region
+}
 resource "azurerm_virtual_network" "stacks-vnet" {
   name                = var.name
-  location            = var.region
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = var.address_space
   dns_servers         = var.dns_servers
 }
