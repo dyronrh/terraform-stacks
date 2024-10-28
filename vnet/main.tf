2c395a31853755bb16690e93e05f99a7b2ae1ab9
dyronrh <dyronrh@yahoo.es>
2024-10-28 01:07:00 -0300

    add name_suffix

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = "rg-stacks-${var.resource_group_name}-${var.name_suffix}"
+  name     = "rg-stacks-demo-${var.name_suffix}"
   location = var.region
 }
 resource "azurerm_virtual_network" "stacks-vnet" {
-  name                = var.name
+  name                = "vnet-${var.name_suffix}"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = var.address_space
