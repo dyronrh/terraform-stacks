720130863d4127dc1ffd9e9274cb68c1e11a3a88
dyronrh <dyronrh@yahoo.es>
2024-10-28 09:21:17 -0300

    add new credentials

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -8,11 +8,6 @@ terraform {
   }
 }
 
-variable "location" {
-  type        = string
-  description = "Azure location"
-}
-
 variable "name_suffix" {
   type        = string
   description = "Name suffix for resources"
